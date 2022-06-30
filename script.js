41a278cd6812e33b4cc090aebbac7f853c52a4ef
devyad24 <devyadav4211@gmail.com>
2022-07-01 00:50:02 +0530

    Got the popup working.

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
-function Book(title,author,pages,status){
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.status = status
-    this.info = ()=>{
-        return `${title} by ${author}, ${pages} pages, ${status}`;
-    }
-}
+// function Book(title,author,pages,status){
+//     this.title = title
+//     this.author = author
+//     this.pages = pages
+//     this.status = status
+//     this.info = ()=>{
+//         return `${title} by ${author}, ${pages} pages, ${status}`;
+//     }
+// }
 
-const theHobbit = new Book("The Hobbit","J.R.R Tolkien",295,"not read yet");
-console.log(theHobbit.info());
+// const theHobbit = new Book("The Hobbit","J.R.R Tolkien",295,"not read yet");
+// console.log(theHobbit.info());
 
-function addBookToLibrary(){
+// function addBookToLibrary(){
     
-}
+// }
+const addBookBtn = document.querySelector("#book_btn");
+const popup = document.querySelector("#popup");
+const popupOverlay = document.querySelector("#popup-overlay");
+const exitFormbtn = document.querySelector("#exitbtn");
+
+exitFormbtn.addEventListener("click",function(){
+    popup.classList.toggle("closed");
+    popupOverlay.classList.toggle("closed");
+});
+
+addBookBtn.addEventListener("click",function(){
+    popup.classList.toggle("closed");
+    popupOverlay.classList.toggle("closed");
+    popup.style.zIndex = "200";
+    popupOverlay.style.zIndex = "150";
+});
