2b88e88cb456c4a2ddc82768e18bc364fde87e33
devyad24 <devyadav4211@gmail.com>
2022-07-16 13:36:35 +0530

    Dynamically change book stats on basis of select values.

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-function Book(title,author,pages,status){
+function Book(title, author, pages, status) {
     this.title = title
     this.author = author
     this.pages = pages
     this.status = status
-    this.info = ()=>{
+    this.info = () => {
         return `${title} by ${author}, ${pages} pages, ${status}`;
     }
 }
@@ -18,19 +18,20 @@ let readBooks = 0;
 let wishlistBooks = 0;
 let shelfBooks = 0;
 
-function exitForm(e){
+function exitForm(e) {
     popup.classList.toggle("opened");
     popupOverlay.classList.toggle("opened");
     popup.style.zindex = "-200";
     popupOverlay.style.zindex = "-250";
     let books = document.querySelectorAll(`div[data-book]`);
-    books.forEach( b => b.remove());
+    books.forEach(b => b.remove());
     displayBooks();
+    // allSelectElements = document.querySelectorAll('[data-selector="yes"]');
 }
 
-exitFormbtn.addEventListener("click",exitForm);
+exitFormbtn.addEventListener("click", exitForm);
 
-addBookBtn.addEventListener("click",()=>{
+addBookBtn.addEventListener("click", () => {
     popup.classList.toggle("opened");
     popupOverlay.classList.toggle("opened");
     popup.style.zIndex = "200";
@@ -49,96 +50,150 @@ let myForm = document.querySelector("#myForm");
 
 const allBooksDiv = document.querySelector(".books");
 
-submitFormbtn.addEventListener("click",(e)=>{
+submitFormbtn.addEventListener("click", (e) => {
     e.preventDefault();
-    let newBook = new Book(bookTitle.value,bookAuthor.value,bookPages.value,bookStatus.value);
-    if(bookTitle.value!==""){
-    myLibrary.push(newBook);
-    } 
+    let newBook = new Book(bookTitle.value, bookAuthor.value, bookPages.value, bookStatus.value);
+    if (bookTitle.value !== "") {
+        myLibrary.push(newBook);
+    }
     exitForm();
     myForm.reset();
 
-    if(newBook.status==="read"){
+    if (newBook.status === "read") {
         readBooks++;
         statusRead.textContent = readBooks;
     }
-    else if(newBook.status==="wantToRead"){
+    else if (newBook.status === "wantToRead") {
         wishlistBooks++;
-        statusWishlist.textContent = wishlistBooks; 
+        statusWishlist.textContent = wishlistBooks;
     }
-    else{
+    else {
         shelfBooks++;
-        statusShelf.textContent = shelfBooks; 
+        statusShelf.textContent = shelfBooks;
     }
 })
 
-function displayBooks(){
-myLibrary.forEach(book =>{
-    let bookDiv = document.createElement("div");
-    bookDiv.setAttribute(`data-book`,`${myLibrary.indexOf(this)}`);
-    bookDiv.classList.add("book");
-    let displayTitle = document.createElement("p");
-    let displayAuthor = document.createElement("p");
-    let displayPages = document.createElement("p");
-    let displayStatus = document.createElement("p");
-    let statusSelect = document.createElement("select");
-    statusSelect.setAttribute("id","readStatus");
-    statusSelect.setAttribute("name","status");
-    let optionRead = document.createElement("option");
-    let optionWantTo = document.createElement("option");
-    let optionShelf = document.createElement("option");
-    let removeBook = document.createElement("button");
-    removeBook.textContent = "Remove";
-    removeBook.classList.add("remove");
-    removeBook.addEventListener('click',(e)=>{
-        let removeAttributeNum = e.target; 
-        let deletedBook = removeAttributeNum.parentNode.parentNode; 
-        myLibrary.splice(myLibrary.indexOf(book),1);
-        if(removeAttributeNum.parentNode.firstChild.value==="read"){
-            readBooks--;
-            statusRead.textContent = readBooks;
+let previousSelectVal = "";
+
+function displayBooks() {
+    myLibrary.forEach(book => {
+        let bookDiv = document.createElement("div");
+        bookDiv.setAttribute(`data-book`, `${myLibrary.indexOf(this)}`);
+        bookDiv.classList.add("book");
+        let displayTitle = document.createElement("p");
+        let displayAuthor = document.createElement("p");
+        let displayPages = document.createElement("p");
+        let displayStatus = document.createElement("p");
+        let statusSelect = document.createElement("select");
+        statusSelect.setAttribute("data-selector", "yes");
+        statusSelect.setAttribute("name", "status");
+        let optionRead = document.createElement("option");
+        let optionWantTo = document.createElement("option");
+        let optionShelf = document.createElement("option");
+        let removeBook = document.createElement("button");
+        removeBook.textContent = "Remove";
+        removeBook.classList.add("remove");
+        removeBook.addEventListener('click', (e) => {
+            let removeAttributeNum = e.target;
+            let deletedBook = removeAttributeNum.parentNode.parentNode;
+            myLibrary.splice(myLibrary.indexOf(book), 1);
+            if (removeAttributeNum.parentNode.firstChild.value === "read") {
+                readBooks--;
+                statusRead.textContent = readBooks;
+            }
+            else if (removeAttributeNum.parentNode.firstChild.value === "wantToRead") {
+                wishlistBooks--;
+                statusWishlist.textContent = wishlistBooks;
+            }
+            else if (removeAttributeNum.parentNode.firstChild.value === "addToShelf") {
+                shelfBooks--;
+                statusShelf.textContent = shelfBooks;
+            }
+            deletedBook.remove();
+        })
+        optionRead.value = "read";
+        optionRead.textContent = "Read";
+        optionWantTo.value = "wantToRead";
+        optionWantTo.textContent = "Want to read";
+        optionShelf.value = "addToShelf";
+        optionShelf.textContent = "Add to shelf";
+        statusSelect.append(optionRead);
+        statusSelect.append(optionWantTo);
+        statusSelect.append(optionShelf);
+        displayStatus.append(statusSelect);
+        displayStatus.append(removeBook);
+        displayTitle.textContent = book.title;
+        displayAuthor.textContent = book.author;
+        displayPages.textContent = book.pages;
+        if (book.status === 'read') {
+            optionRead.setAttribute("selected", "selected");
         }
-        else if(removeAttributeNum.parentNode.firstChild.value==="wantToRead"){
-            wishlistBooks--;
-            statusWishlist.textContent = wishlistBooks;
+        else if (book.status === 'wantToRead') {
+            optionWantTo.setAttribute("selected", "selected");
         }
-        else if(removeAttributeNum.parentNode.firstChild.value==="addToShelf"){
-            shelfBooks--;
-            statusShelf.textContent = shelfBooks;
+        else {
+            optionShelf.setAttribute("selected", "selected");
         }
-        deletedBook.remove();
+        bookDiv.append(displayTitle);
+        bookDiv.append(displayAuthor);
+        bookDiv.append(displayPages);
+        bookDiv.append(displayStatus);
+        allBooksDiv.append(bookDiv);
+        statusSelect.addEventListener('focus',e => previousSelectVal = e.target.value);
+        statusSelect.addEventListener('change',manageBookStats);
     })
-    optionRead.value = "read";
-    optionRead.textContent = "Read";
-    optionWantTo.value = "wantToRead";
-    optionWantTo.textContent = "Want to read";
-    optionShelf.value = "addToShelf";
-    optionShelf.textContent = "Add to shelf";
-    statusSelect.append(optionRead);
-    statusSelect.append(optionWantTo);
-    statusSelect.append(optionShelf);
-    displayStatus.append(statusSelect);
-    displayStatus.append(removeBook);
-    displayTitle.textContent = book.title;
-    displayAuthor.textContent = book.author;
-    displayPages.textContent = book.pages;
-    if(book.status==='read'){
-        optionRead.setAttribute("selected","selected");
-    }
-    else if(book.status==='wantToRead'){
-        optionWantTo.setAttribute("selected","selected");
-    }
-    else{
-        optionShelf.setAttribute("selected","selected");
-    }
-    bookDiv.append(displayTitle);
-    bookDiv.append(displayAuthor);
-    bookDiv.append(displayPages);
-    bookDiv.append(displayStatus);
-    allBooksDiv.append(bookDiv);
-})
 }
 
 let statusRead = document.querySelector("#stat_read");
 let statusWishlist = document.querySelector("#stat_wishlist");
 let statusShelf = document.querySelector("#stat_shelf");
+
+// let allSelectElements;
+
+// function manipBookStats() {
+//     if (allSelectElements !== undefined) {
+
+//         allSelectElements.forEach(currentSelect => {
+//             currentSelect.addEventListener('change', manageBookStats)
+//         })
+//     }
+// }
+function manageBookStats(e) {
+    if (e.target.value === "read" && previousSelectVal === "wantToRead") {
+        readBooks++;
+        wishlistBooks--;
+        statusRead.textContent = readBooks;
+        statusWishlist.textContent = wishlistBooks;
+    }
+    else if (e.target.value === "wantToRead" && previousSelectVal === "read") {
+        readBooks--;
+        wishlistBooks++;
+        statusRead.textContent = readBooks;
+        statusWishlist.textContent = wishlistBooks;
+    }
+    else if (e.target.value === "addToShelf" && previousSelectVal === "read") {
+        readBooks--;
+        shelfBooks++;
+        statusShelf.textContent = shelfBooks;
+        statusRead.textContent = readBooks;
+    }
+    else if (e.target.value === "read" && previousSelectVal === "addToShelf") {
+        readBooks++;
+        shelfBooks--;
+        statusRead.textContent = readBooks;
+        statusShelf.textContent = shelfBooks;
+    }
+    else if (e.target.value === "wantToRead" && previousSelectVal === "addToShelf") {
+        wishlistBooks++;
+        shelfBooks--;
+        statusWishlist.textContent = wishlistBooks;
+        statusShelf.textContent = shelfBooks;
+    }
+    else if (e.target.value === "addToShelf" && previousSelectVal === "wantToRead") {
+        shelfBooks++;
+        wishlistBooks--;
+        statusShelf.textContent = shelfBooks;
+        statusWishlist.textContent = wishlistBooks;
+    }
+    previousSelectVal = e.target.value;
+}
